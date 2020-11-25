aef18f004a0c9ccc0bad3d8eb3de77151b4ecc66
Erling Van De Weijer <erling.weijer@gmail.com>
2020-11-25 18:08:40 +0100

    Style kNN page, and add some text to describe the machine learning model as well as the interactive model.

--- ml-demo/src/pages/KNN/index.js
+++ ml-demo/src/pages/KNN/index.js
@@ -2,23 +2,23 @@ import React from "react";
 import Papa from "papaparse";
 import data_csv from "../../assets/ushape.csv";
 import * as d3 from "d3";
+import { Button, TextField } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
 const euclideanDistance = (point1, point2) => {
-  return Math.sqrt(
-    (parseFloat(point1.X) - point2.X) ** 2 +
-      (parseFloat(point1.Y) - point2.Y) ** 2
-  );
+  return Math.sqrt((parseFloat(point1.X) - point2.X) ** 2 + (parseFloat(point1.Y) - point2.Y) ** 2);
 };
 
 const calulateClass = (datapoint, data, k) => {
-    console.log(k);
+  console.log(k);
   let nearestArr = [];
   let nearestClasses = [];
   for (let i = 0; i < k; i++) {
     nearestArr.push(1000000);
     nearestClasses.push(-1);
   }
-  data.forEach(d => {
+  data.forEach((d) => {
     const dist = euclideanDistance(d, datapoint);
     const maxVal = Math.max.apply(null, nearestArr);
     if (dist < maxVal) {
@@ -27,8 +27,8 @@ const calulateClass = (datapoint, data, k) => {
       nearestClasses[ind] = d.class;
     }
   });
-  const cl0 = nearestClasses.filter(p => p == 0);
-  const cl1 = nearestClasses.filter(p => p == 1);
+  const cl0 = nearestClasses.filter((p) => p == 0);
+  const cl1 = nearestClasses.filter((p) => p == 1);
   return cl0 > cl1 ? 0 : 1;
 };
 
@@ -39,7 +39,7 @@ const KNeighbors = () => {
     X: 0.0,
     Y: 0.0,
     class: -1,
-    movable: 1
+    movable: 1,
   });
   const [k, setK] = React.useState(9);
   React.useEffect(() => {
@@ -61,98 +61,129 @@ const KNeighbors = () => {
     plotData(data);
   }, [data, point]);
 
-  const plotData = dataset => {
-    d3.select(svgRef.current)
-      .selectAll("svg")
-      .remove();
-    var svg = d3
-      .select(svgRef.current)
-      .append("svg")
-      .attr("width", 500)
-      .attr("height", 500);
+  const plotData = (dataset) => {
+    d3.select(svgRef.current).selectAll("svg").remove();
+    var svg = d3.select(svgRef.current).append("svg").attr("width", 500).attr("height", 500);
 
     svg
       .selectAll("circle")
       .data(dataset.concat([point]))
       .enter()
       .append("circle")
-      .attr("cx", function(d) {
+      .attr("cx", function (d) {
         return (parseFloat(d.X) + 2) * 100;
       })
-      .attr("stroke", d => d.movable && "rgb(0,0,0)")
-      .attr("stroke-width", d => d.movable && "2")
-      .attr("cy", function(d) {
+      .attr("stroke", (d) => d.movable && "rgb(0,0,0)")
+      .attr("stroke-width", (d) => d.movable && "2")
+      .attr("cy", function (d) {
         return 500 - (parseFloat(d.Y) + 2) * 100;
       })
-      .attr("r", d => (d.movable ? 5 : 3))
-      .attr("fill", function(d) {
-        return d.class == -1
-          ? "rgb(255,255,255)"
-          : d.class == 1
-          ? "rgb(255, 0 ,0)"
-          : "rgb(0, 0, 255)";
+      .attr("r", (d) => (d.movable ? 5 : 3))
+      .attr("fill", function (d) {
+        return d.class == -1 ? "rgb(255,255,255)" : d.class == 1 ? "rgb(255, 0 ,0)" : "rgb(0, 0, 255)";
       });
   };
 
   return (
-    <div>
-      <div>
-        <div style={{ display: "flex" }}>
-          <div ref={svgRef}>
-            <input
-              type="range"
-              min={0}
-              max={500}
-              value={(point.X + 2) * 100}
-              className="slider"
-              id="myRange"
-              style={{ width: "500px" }}
-              onChange={e =>
-                setPoint({
-                  X: e.target.value / 100 - 2,
-                  Y: point.Y,
-                  class: point.class,
-                  movable: point.movable
-                })
-              }
+    <>
+      <Grid container alignItems="center" direction="column" spacing={5}>
+        <Grid item alignItems="right" xs={8}>
+          <Typography align="left">
+            kNN falls within the supervised learning category in machine learning. This means that the algorithm uses a
+            training set to determine a predictive model for new data points. Although kNN is mainly used for
+            classification as in the example above, it may also be used in combination with other machine learning
+            algorithms for regression. kNN is a lazy learning algorithm, which means that there is no explicit training
+            phase. Instead of training a model, kNN uses all of the training data during test phase. The core of the kNN
+            algorithm is simply measuring the distance between tested examples and training examples. Although its
+            simplistic nature, kNN has proven to be a very effective machine learning algorithm
+          </Typography>
+          <Typography align="left">
+            Here we have created an interactive model for testing how kNN works in practice. By moving the scroll bars
+            on the top and right of the grid, it is possible to move the marker. Once the marker is placed, one can
+            choose any value of k. Press calculate to find out wheter the maker will be classified as either blue or
+            red, based on the nearest k neighbors.
+          </Typography>
+        </Grid>
+        <Grid container direction="row" xs={8} justify="center">
+          <input
+            type="range"
+            min={0}
+            max={500}
+            value={(point.X + 2) * 100}
+            className="slider"
+            id="myRange"
+            style={{ width: "500px" }}
+            onChange={(e) =>
+              setPoint({
+                X: e.target.value / 100 - 2,
+                Y: point.Y,
+                class: point.class,
+                movable: point.movable,
+              })
+            }
+          />
+        </Grid>
+        <Grid container direction="row" justify="center">
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <div>
+              <div style={{ display: "flex" }}>
+                <div ref={svgRef}></div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={4} xt={4}>
+            <div style={{ display: "flex", marginTop: "250px", marginLeft: "-200px" }}>
+              <input
+                type="range"
+                min={0}
+                max={500}
+                value={(point.Y + 2) * 100}
+                className="slider"
+                id="myRange"
+                style={{ width: "500px", transform: "rotate(270deg)" }}
+                onChange={(e) =>
+                  setPoint({
+                    X: point.X,
+                    Y: e.target.value / 100 - 2,
+                    class: point.class,
+                    movable: point.movable,
+                  })
+                }
+              />
+            </div>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} direction="row">
+          <Grid item>
+            <TextField
+              label="Value of K"
+              variant="outlined"
+              type={"text"}
+              value={k}
+              onChange={(e) => setK(e.target.value)}
             />
-          </div>
-          <div style={{display: "flex", marginLeft: "-100px"}}>
-            <input
-              type="range"
-              min={0}
-              max={500}
-              value={(point.Y + 2) * 100}
-              className="slider"
-              id="myRange"
-              style={{ width: "500px", transform: "rotate(270deg)" }}
-              onChange={e =>
+          </Grid>
+          <Grid item>
+            <Button
+              top
+              variant="contained"
+              color="primary"
+              onClick={() =>
                 setPoint({
                   X: point.X,
-                  Y: e.target.value / 100 - 2,
-                  class: point.class,
-                  movable: point.movable
+                  Y: point.Y,
+                  class: calulateClass(point, data, k),
+                  movable: true,
                 })
               }
-            />
-          </div>
-        </div>
-      </div>
-        k=
-        <input type={"text"} value={k} onChange={e=>setK(e.target.value)}/>
-      <button
-        onClick={() =>
-          setPoint({
-            X: point.X,
-            Y: point.Y,
-            class: calulateClass(point, data, k),
-            movable: true
-          })
-        }
-      >
-        Kalkuler
-      </button>
-    </div>
+            >
+              Calculate
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
