04f9779b2a8c35998e77bced2d6b1e0719f41ba4
Erling Van De Weijer <erling.weijer@gmail.com>
2020-11-26 12:24:20 +0100

    Add title to KNN page, and fix linting issues.

--- ml-demo/src/pages/KNN/index.js
+++ ml-demo/src/pages/KNN/index.js
@@ -4,13 +4,10 @@ import data_csv from "../../assets/ushape.csv";
 import * as d3 from "d3";
 import { Button, TextField } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-import { Typography } from "@material-ui/core";
+import { Typography, Box } from "@material-ui/core";
 
 const euclideanDistance = (point1, point2) => {
-  return Math.sqrt(
-    (parseFloat(point1.X) - point2.X) ** 2 +
-      (parseFloat(point1.Y) - point2.Y) ** 2
-  );
+  return Math.sqrt((parseFloat(point1.X) - point2.X) ** 2 + (parseFloat(point1.Y) - point2.Y) ** 2);
 };
 
 const calulateClass = (datapoint, data, k) => {
@@ -20,7 +17,7 @@ const calulateClass = (datapoint, data, k) => {
     nearestArr.push(1000000);
     nearestClasses.push(-1);
   }
-  data.forEach(d => {
+  data.forEach((d) => {
     const dist = euclideanDistance(d, datapoint);
     const maxVal = Math.max.apply(null, nearestArr);
     if (dist < maxVal) {
@@ -29,12 +26,9 @@ const calulateClass = (datapoint, data, k) => {
       nearestClasses[ind] = d.class;
     }
   });
-  const cl0 = nearestClasses.filter(p => p == 0);
-  const cl1 = nearestClasses.filter(p => p == 1);
-  return [
-    cl0.length > cl1.length ? 0 : cl0.length === cl1.length ? -1 : 1,
-    Math.max.apply(null, nearestArr)
-  ];
+  const cl0 = nearestClasses.filter((p) => p == 0);
+  const cl1 = nearestClasses.filter((p) => p == 1);
+  return [cl0.length > cl1.length ? 0 : cl0.length === cl1.length ? -1 : 1, Math.max.apply(null, nearestArr)];
 };
 
 const KNeighbors = () => {
@@ -45,7 +39,7 @@ const KNeighbors = () => {
     X: 0.0,
     Y: 0.0,
     class: -1,
-    movable: 1
+    movable: 1,
   });
   const [k, setK] = React.useState(1);
   React.useEffect(() => {
@@ -67,42 +61,27 @@ const KNeighbors = () => {
     plotData(data);
   }, [data, point]);
 
-  const plotData = dataset => {
-    d3.select(svgRef.current)
-      .selectAll("svg")
-      .remove();
-    var svg = d3
-      .select(svgRef.current)
-      .append("svg")
-      .attr("width", 500)
-      .attr("height", 500);
+  const plotData = (dataset) => {
+    d3.select(svgRef.current).selectAll("svg").remove();
+    var svg = d3.select(svgRef.current).append("svg").attr("width", 500).attr("height", 500);
 
     svg
       .selectAll("circle")
-      .data(
-        dataset.concat([
-          point,
-          { X: point.X, Y: point.Y, class: point.class, maxVal: maxVal }
-        ])
-      )
+      .data(dataset.concat([point, { X: point.X, Y: point.Y, class: point.class, maxVal: maxVal }]))
       .enter()
       .append("circle")
-      .attr("cx", function(d) {
+      .attr("cx", function (d) {
         return (parseFloat(d.X) + 2) * 100;
       })
-      .attr("stroke", d => d.movable && "rgb(0,0,0)")
-      .attr("stroke-width", d => d.movable && "2")
-      .attr("cy", function(d) {
+      .attr("stroke", (d) => d.movable && "rgb(0,0,0)")
+      .attr("stroke-width", (d) => d.movable && "2")
+      .attr("cy", function (d) {
         return 500 - (parseFloat(d.Y) + 2) * 100;
       })
-      .attr("r", d => (d.movable ? 5 : d.maxVal ? d.maxVal : 3))
-      .attr("opacity", d => (d.maxVal ? 0.1 : 1))
-      .attr("fill", function(d) {
-        return d.class == -1
-          ? "rgb(255,255,255)"
-          : d.class == 1
-          ? "rgb(255, 0 ,0)"
-          : "rgb(0, 0, 255)";
+      .attr("r", (d) => (d.movable ? 5 : d.maxVal ? d.maxVal : 3))
+      .attr("opacity", (d) => (d.maxVal ? 0.1 : 1))
+      .attr("fill", function (d) {
+        return d.class == -1 ? "rgb(255,255,255)" : d.class == 1 ? "rgb(255, 0 ,0)" : "rgb(0, 0, 255)";
       });
   };
 
@@ -112,36 +91,32 @@ const KNeighbors = () => {
       X: point.X,
       Y: point.Y,
       class: calcClass[0],
-      movable: true
+      movable: true,
     });
     setMaxVal(calcClass[1] * 100);
   };
 
   return (
     <>
+      <Box>
+        <h1>K-Nearest Neighbors</h1>
+      </Box>
       <Grid container alignItems="center" direction="column" spacing={5}>
         <Grid item alignItems="right" xs={8}>
           <Typography align="left">
-            k-Nearest Neighbors (kNN) falls within the supervised learning
-            category in machine learning. This means that the algorithm uses a
-            training set to determine a predictive model for new data points.
-            Although kNN is mainly used for classification as in the example
-            below, it may also be used in combination with other machine
-            learning algorithms for regression. kNN is a lazy learning
-            algorithm, which means that there is no explicit training phase.
-            Instead of training a model, kNN uses all of the training data
-            during test phase. The core of the kNN algorithm is simply measuring
-            the distance between tested examples and training examples. Although
-            its simplistic nature, kNN has proven to be a very effective machine
-            learning algorithm
+            k-Nearest Neighbors (kNN) falls within the supervised learning category in machine learning. This means that
+            the algorithm uses a training set to determine a predictive model for new data points. Although kNN is
+            mainly used for classification as in the example below, it may also be used in combination with other
+            machine learning algorithms for regression. kNN is a lazy learning algorithm, which means that there is no
+            explicit training phase. Instead of training a model, kNN uses all of the training data during test phase.
+            The core of the kNN algorithm is simply measuring the distance between tested examples and training
+            examples. Although its simplistic nature, kNN has proven to be a very effective machine learning algorithm
           </Typography>
           <Typography align="left">
-            Here we have created an interactive model for testing how kNN works
-            in practice. By moving the scroll bars on the top and right of the
-            grid, it is possible to move the marker. Once the marker is placed,
-            one can choose any positive value for k. Press calculate to find out
-            whether the marker will be classified as either blue or red, based
-            on the nearest k neighbors.
+            Here we have created an interactive model for testing how kNN works in practice. By moving the scroll bars
+            on the top and right of the grid, it is possible to move the marker. Once the marker is placed, one can
+            choose any positive value for k. Press calculate to find out whether the marker will be classified as either
+            blue or red, based on the nearest k neighbors.
           </Typography>
         </Grid>
         <Grid container direction="row" xs={8} justify="center">
@@ -153,12 +128,12 @@ const KNeighbors = () => {
             className="slider"
             id="myRange"
             style={{ width: "500px" }}
-            onChange={e =>
+            onChange={(e) =>
               setPoint({
                 X: e.target.value / 100 - 2,
                 Y: point.Y,
                 class: point.class,
-                movable: point.movable
+                movable: point.movable,
               })
             }
           />
@@ -177,7 +152,7 @@ const KNeighbors = () => {
               style={{
                 display: "flex",
                 marginTop: "250px",
-                marginLeft: "-200px"
+                marginLeft: "-200px",
               }}
             >
               <input
@@ -188,12 +163,12 @@ const KNeighbors = () => {
                 className="slider"
                 id="myRange"
                 style={{ width: "500px", transform: "rotate(270deg)" }}
-                onChange={e =>
+                onChange={(e) =>
                   setPoint({
                     X: point.X,
                     Y: e.target.value / 100 - 2,
                     class: point.class,
-                    movable: point.movable
+                    movable: point.movable,
                   })
                 }
               />
@@ -207,16 +182,11 @@ const KNeighbors = () => {
               variant="outlined"
               type={"number"}
               value={k}
-              onChange={e => setK(e.target.value)}
+              onChange={(e) => setK(e.target.value)}
             />
           </Grid>
           <Grid item>
-            <Button
-              top
-              variant="contained"
-              color="primary"
-              onClick={() => calculate()}
-            >
+            <Button top variant="contained" color="primary" onClick={() => calculate()}>
               Calculate
             </Button>
           </Grid>
