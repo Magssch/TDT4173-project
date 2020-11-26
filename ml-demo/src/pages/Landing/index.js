977ae89a5d84827909e91ff69d4c3454ec57d663
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2020-11-26 21:46:21 +0100

    Fix page width

--- ml-demo/src/pages/Landing/index.js
+++ ml-demo/src/pages/Landing/index.js
@@ -5,7 +5,7 @@ import { Box } from '@material-ui/core';
 const Landing = () =>{
     return (
         <Box my={5} display={"flex"} flexDirection={"column"}>
-            <Box mx={"auto"} style={{maxWidth: "800px"}}>
+            <Box mx={"auto"} style={{maxWidth: "850px"}}>
                 <h1 style={{color: "#222426"}}>What characterizes a good question on Stack Overflow?</h1>
                 <p>For our final project in TDT4173 Machine Learning at NTNU, we have attempted to find an answer to this question. We have not found an answer, although, by using 45k previously asked questions on stackoverflow.com, we have trained a model that might shed some light on the characteristics of a high quality question, and that of a low quality and unfocused question.</p>
                 <p>In the header you can find links to a general demonstration of k-Nearest Neighbors, as well as a live demonstration of question quality prediction using a Recurrent Neural Network with Long Short-Term Memory.
