124378ec324262e7b538f79abcccec9a569c907e
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-03-29 12:26:51 +0200

    Update firebase function to load TF model directly

--- ml-demo/functions/src/index.ts
+++ ml-demo/functions/src/index.ts
@@ -1,45 +1,61 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 const cors = require("cors")({
-    origin: true,
-    allowedHeaders: [
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Methods",
-      "Content-Type",
-      "Origin",
-      "X-Requested-With",
-      "Accept"
-    ],
-    methods: ["POST"],
-    credentials: true
-  });
-
-import { google } from 'googleapis';
-const ml = google.ml('v1')
-
-export const getQuestionQuality = functions.https.onRequest(async (request, response) => {
-
-    return cors(request, response, async () => {
-        response.set("Content-Type", "Application/JSON");
-        response.set("Access-Control-Allow-Origin", "https://tdt4173-project-sl-1.web.app");
-        response.set("Vary", "Origin");
-        const instances = JSON.parse(request.body).instances; 
-
-        const { credential } = await google.auth.getApplicationDefault();
-        const modelName = `projects/tdt4173-project-sl-1/models/rnn_lstm`;
-        console.log("Getting instances:" + instances)
-        ml.projects.predict({
-            auth: credential,
-            name: modelName,
-            requestBody: { 
-                instances,
-            },
-        } as any, {
-            rootUrl: "https://europe-west4-ml.googleapis.com",
-        } as any).then(
-            res => {
-                console.log(res);
-                return response.send(JSON.stringify(res.data))
-            }
-        ).catch(err => response.send(JSON.stringify(err)));
-    })
+  origin: true,
+  allowedHeaders: [
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Origin",
+    "X-Requested-With",
+    "Accept",
+  ],
+  methods: ["POST"],
+  credentials: true,
 });
+
+const tf = require("@tensorflow/tfjs-node");
+
+let objectDetectionModel: { predict: (arg0: any) => any };
+async function loadModel(input: any) {
+  // Warm up the model
+  if (!objectDetectionModel) {
+    // Load the TensorFlow SavedModel through tfjs-node API. You can find more
+    // details in the API documentation:
+    // https://js.tensorflow.org/api_node/1.3.1/#node.loadSavedModel
+    objectDetectionModel = await tf.node.loadSavedModel(
+      "model",
+      ["serve"],
+      "serving_default"
+    );
+  }
+  var res = null;
+  try {
+    var input_data = tf.tensor(input);
+    var predictions = objectDetectionModel.predict(input_data);
+    res = predictions.array().then((array: any[]) => array);
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+  return res;
+}
+
+export const getQuestionQuality = functions.https.onRequest(
+  async (request, response) => {
+    return cors(request, response, async () => {
+      response.set("Content-Type", "Application/JSON");
+      response.set("Access-Control-Allow-Origin", "*");
+      response.set("Vary", "Origin");
+      const instances = JSON.parse(request.body).instances;
+
+      loadModel(instances)
+        .then((r) => {
+          console.log("Prediction done");
+          return response.send(JSON.stringify({ predictions: r }));
+        })
+        .catch((e) => {
+          response.sendStatus(e);
+        });
+    });
+  }
+);
